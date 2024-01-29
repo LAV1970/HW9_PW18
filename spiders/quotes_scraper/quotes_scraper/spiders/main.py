2486df142dd7b151be89f945a7eacafd52ab5315
lomakina0612 <lomakina0612@gmail.com>
2024-01-29 09:24:31 +0100

    2901_21

--- spiders/quotes_scraper/quotes_scraper/spiders/main.py
+++ spiders/quotes_scraper/quotes_scraper/spiders/main.py
@@ -1,6 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from quotes_scraper.spiders.quotes_spider import QuotesSpider
+from quotes_spider import QuotesSpider
+
 
 if __name__ == "__main__":
     process = CrawlerProcess(get_project_settings())
